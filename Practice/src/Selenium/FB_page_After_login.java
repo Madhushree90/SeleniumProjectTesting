dab24fc0c0d07f5bed53e1d01dd73572c3051a4c
madhushree90 <madhu.megharaj@gmail.com>
2020-04-16 19:55:56 +0530

    First commit

---
+++ Practice/src/Selenium/FB_page_After_login.java
@@ -0,0 +1,132 @@
+package Selenium;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+@SuppressWarnings("unused")
+public class FB_page_After_login {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+System.setProperty("webdriver.chrome.driver", "C:\\W2\\chromedriver.exe");
+
+WebDriver driver = new ChromeDriver();
+
+driver.manage().window().maximize();
+
+// hit below URL
+driver.get("https://m.facebook.com/");
+ 
+
+//FB login page
+Thread.sleep(1000);
+driver.findElement(By.xpath("//input[@name='email']")).sendKeys("8073469494");
+
+Thread.sleep(1000);
+driver.findElement(By.xpath("//input[@id='m_login_password']")).sendKeys("Sharadha@123");
+
+Thread.sleep(1000);
+driver.findElement(By.xpath("//button[@name='login']")).click();
+
+//Log in with one tap- selected not now
+
+Thread.sleep(4000);
+driver.findElement(By.linkText("Not Now")).click();
+
+//get the title of the current page
+String title= driver.getTitle();
+
+//validate if we are landing on correct URL
+System.out.println(driver.getCurrentUrl());
+//Validation 
+if(title.equals("Facebook"))
+{
+	System.out.println("correct title");
+}
+else
+{
+	System.out.println("wrong title");
+	}
+
+// Friend request tab
+Thread.sleep(4000);
+
+driver.findElement(By.xpath("//*[@id='u_0_b']/div/div")).click();
+Thread.sleep(4000);
+//see some suggestions
+//Thread.sleep(4000);
+//driver.findElement(By.xpath("//*[@id='u_0_c']/div/div")).click();
+String Str1 = driver.getCurrentUrl();
+if(Str1.equals("https://m.facebook.com/friends/center/requests/?rfj"))
+{
+	System.out.println("correct Find Friends page");
+}
+else
+{
+	System.out.println("wrong page");
+	}
+//Thread.sleep(4000);
+driver.navigate().back();
+
+//driver.findElement(By.linkText("Find Friends")).click();
+
+//Messages tab
+Thread.sleep(4000);
+driver.findElement(By.xpath("//*[@id='u_0_c']/div/div")).click();
+Thread.sleep(4000);
+
+String Str2 = driver.getCurrentUrl();
+if(Str2.equals("https://m.facebook.com/?soft=messages"))
+{
+	System.out.println("correct Message page");
+}
+else
+{
+	System.out.println("wrong page");
+	}
+driver.navigate().back();
+
+//Notification tab
+
+Thread.sleep(4000);
+driver.findElement(By.xpath("//*[@id='u_0_d']/div/div")).click();
+Thread.sleep(4000);
+String Str3 = driver.getCurrentUrl();
+if(Str3.equals("https://m.facebook.com/?soft=notifications"))
+{
+	System.out.println("correct Notification page");
+}
+else
+{
+	System.out.println("wrong page");
+	}
+driver.navigate().back();
+
+//search
+
+Thread.sleep(4000);
+driver.findElement(By.xpath("//*[@id='search_jewel']/a/div/div")).click();
+Thread.sleep(4000);
+
+
+String Str4 = driver.getCurrentUrl();
+if(Str4.equals("https://m.facebook.com/?soft=search"))
+{
+	System.out.println("correct Search page");
+}
+else
+{
+	System.out.println("wrong page");
+	}
+//driver.navigate().back();
+
+//search for friend
+driver.findElement(By.xpath("//input[@name='query']")).sendKeys("sinchana J N" + "\n");
+
+
+
+
+
+	}
+
+}
